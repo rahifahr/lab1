f0ed94d94cc6aac58d251218b576d450343753a9
Rahifah Rahman <rahifah@bu.edu>
2026-09-18 13:05:00 -0400

    button changes while in scary mode

--- script.js
+++ script.js
@@ -5,12 +5,15 @@ function toggleScary() {
 if (!isScary) {
         document.getElementById("cute-content").style.display = "none";
         document.getElementById("scary-content").style.display = "block";
+        document.getElementById("scary-button").innerHTML = "☠ ESCAPE THE MONSTERS! ☠";
         isScary = true;
-    } else {
+} else {
         document.getElementById("cute-content").style.display = "block";
         document.getElementById("scary-content").style.display = "none";
+        document.getElementById("scary-button").innerHTML = "☠ ENTER IF YOU DARE! ☠";
         isScary = false;
     }
 }
 
 document.getElementById("scary-button").onclick = toggleScary;
+
